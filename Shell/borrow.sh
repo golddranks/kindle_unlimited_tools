874c6367dfa01d3a7794b8f72e0e8a06572d4542
Pyry Kontio <pyry.kontio@drasa.eu>
2025-03-19 03:41:53 +0900

    borrow yea

--- Shell/borrow.sh
+++ Shell/borrow.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+buffer=20
+KINDLE_EXPORT_PATH=${KINDLE_EXPORT_PATH:?}
+exported=$(find "$KINDLE_EXPORT_PATH" | wc -l)
+borrowed=$exported
+echo "Borrowed: $borrowed Exported: $exported"
+
 while :; do
-    for i in $(seq 10); do
-        read line || exit 0
-        echo "https://www.amazon.co.jp/dp/$line?borrow"
-        open "https://www.amazon.co.jp/dp/$line?borrow"
-    done
-    sleep 240
     date -R
+    exported=$(find "$KINDLE_EXPORT_PATH" | wc -l)
+    while [ "$borrowed" -ge "$((exported + buffer))" ]; do
+        sleep 5
+    done
+    echo "Borrowed: $borrowed Exported: $exported"
+    read -r line || exit 0
+    echo "https://www.amazon.co.jp/dp/$line?borrow"
+    open "https://www.amazon.co.jp/dp/$line?borrow"
+    borrowed=$((borrowed+1))
+    echo "Borrowed: $borrowed Exported: $exported"
+    sleep 60
 done
