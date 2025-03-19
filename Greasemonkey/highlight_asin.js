a7fe76e1fbc96c1f0014dd8e03e9646402a72a26
Pyry Kontio <pyry.kontio@drasa.eu>
2025-03-20 02:32:48 +0900

    Better highlights

--- Greasemonkey/highlight_asin.js
+++ Greasemonkey/highlight_asin.js
@@ -9,12 +9,24 @@ const asins = `B07168N9Q7
 B0CBLV4N9M`; // For example
 
 const asinRegex = /B0[0-9A-Z]{8}/g;
-const comicsRegex = /コミックス|Comics|comics|COMICS/gu;
+const comicsRegex = /コミック|Comics|comics|COMICS/gu;
 
-const urlMatches = document.URL.match(asinRegex);
-if (urlMatches) {
-  if (urlMatches.some((m) => asins.includes(m))) {
+const urlAsinMatches = document.URL.match(asinRegex);
+const urlComicMatches = document.URL.match(comicsRegex);
+if (urlAsinMatches) {
+  if (urlAsinMatches.some((m) => asins.includes(m))) {
     document.body.setAttribute("style", "background-color: yellow;");
+  }
+}
+if (urlComicMatches) {
+  document.body.setAttribute("style", "background-color: red;");
+}
+
+for (const h1 of document.getElementsByTagName("h1")) {
+  const headerComicMatches = h1.textContent.match(comicsRegex);
+  console.log("h1", h1, h1.textContent, headerComicMatches);
+  if (headerComicMatches) {
+    h1.setAttribute("style", "background-color: red;");
   }
 }
 
@@ -22,19 +34,21 @@ const perform = () => {
   for (const a of document.getElementsByTagName("a")) {
     const hrefMatches = a.href.match(asinRegex);
     const comicMatches = a.textContent.match(comicsRegex);
-    if (hrefMatches == null && comicMatches == null) {
-      continue;
-    }
+    console.log(a, a.textContent);
     if (comicMatches) {
+      a.setAttribute("style", "background-color: red;");
       for (const text of a.getElementsByTagName("*")) {
         const textMatches = text.textContent.match(comicsRegex);
+        console.log(text, textMatches, text.textContent);
         if (textMatches) {
           text.setAttribute("style", "background-color: red;");
         }
       }
     }
-    if (hrefMatches.some((m) => asins.includes(m))) {
-      a.setAttribute("style", "background-color: yellow;");
+    if (hrefMatches) {
+      if (hrefMatches.some((m) => asins.includes(m))) {
+        a.setAttribute("style", "background-color: yellow;");
+      }
     }
   }
 };
